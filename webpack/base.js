10caf19367634cc0631c86f25251ed7b24ba406b
amplee <liyanping@corp.netease.com>
2021-02-08 17:36:34 +0800

    feat:游戏初始化

---
+++ webpack/base.js
@@ -0,0 +1,47 @@
+/*
+ * @Descripttion: 
+ * @Author: amplee
+ * @Date: 2021-02-08 11:55:16
+ * @LastEditors: amplee
+ * @LastEditTime: 2021-02-08 14:26:39
+ */
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: "raw-loader"
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml|mp3)$/i,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, "../")
+    }),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
+};
